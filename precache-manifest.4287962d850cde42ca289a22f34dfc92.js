922a476784b1f3e24fbb891751b69af89196678e
junebug <47231340+junebug111@users.noreply.github.com>
2020-07-12 13:23:15 -0400

    Updates

---
+++ precache-manifest.4287962d850cde42ca289a22f34dfc92.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39bfdea6d45e32f85eb353f4152d5109",
+    "url": "/myblog/index.html"
+  },
+  {
+    "revision": "c2b24b68168afa731358",
+    "url": "/myblog/static/css/main.ddae1749.chunk.css"
+  },
+  {
+    "revision": "fd57ecb702af5dd4e877",
+    "url": "/myblog/static/js/2.ca3bebcb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myblog/static/js/2.ca3bebcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2b24b68168afa731358",
+    "url": "/myblog/static/js/main.f47d2548.chunk.js"
+  },
+  {
+    "revision": "5c7dce129b8c407747ce",
+    "url": "/myblog/static/js/runtime-main.58801b33.js"
+  },
+  {
+    "revision": "0df1ab6cd98dd6974b5cb0130a60f82b",
+    "url": "/myblog/static/media/Resume.0df1ab6c.pdf"
+  },
+  {
+    "revision": "6a8622fd6f7d8e6fab653c1f80db5893",
+    "url": "/myblog/static/media/logo.6a8622fd.png"
+  },
+  {
+    "revision": "5dee1f1296fa72d5aba8e1ce55491d61",
+    "url": "/myblog/static/media/me.5dee1f12.JPG"
+  }
+]);
